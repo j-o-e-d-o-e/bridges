e686b835b9fbd447358165306c3cf0d44cfd48df
j-o-e-d-o-e <d_joe@gmx.net>
2019-01-02 21:18:20 +0100

    minor changes

--- src/net/joedoe/logics/AutoSolverListener.java
+++ src/net/joedoe/logics/AutoSolverListener.java
@@ -3,6 +3,7 @@ package net.joedoe.logics;
 /**
  * Listener für {@link net.joedoe.logics.AutoSolver}, um Daten an View zurückzugeben.
  */
+@FunctionalInterface
 public interface AutoSolverListener {
 
     void onChange();

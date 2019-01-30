2ae5f316585103883350e313445fa2ec2bffeced
j-o-e-d-o-e <d_joe@gmx.net>
2019-01-30 03:38:41 +0100

    howto initially implemented

--- src/net/joedoe/views/HowTo.java
+++ src/net/joedoe/views/HowTo.java
@@ -2,9 +2,16 @@ package net.joedoe.views;
 
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+
+import java.io.File;
+
+import static net.joedoe.utils.GameInfo.CONTAINER_OFFSET;
 
 class HowTo extends BorderPane {
 
@@ -17,11 +24,23 @@ class HowTo extends BorderPane {
     }
 
     private Node setLayout() {
-//        TableView
+        VBox box = new VBox();
+        box.setAlignment(Pos.CENTER);
+        box.setSpacing(CONTAINER_OFFSET);
+        String url = "file:assets" + File.separator + "images" + File.separator + "add.gif";
+        Image addBridge = new Image(url);
+        ImageView view = new ImageView(addBridge);
 
-        Button ok = new Button("Ok");
-        ok.setOnAction(e -> System.out.println("HOWTO!"));
-        setCenter(ok);
-        return ok;
+        HBox hBox = new HBox();
+        Button prev = new Button("Prev");
+        prev.setOnAction(e -> System.out.println("Prev"));
+        hBox.getChildren().addAll();
+        Region regionMiddle = new Region();
+        HBox.setHgrow(regionMiddle, Priority.ALWAYS);
+        Button next = new Button("Next");
+        next.setOnAction(e -> System.out.println("Next"));
+        hBox.getChildren().addAll(prev, regionMiddle, next);
+        box.getChildren().addAll(view, hBox);
+        return box;
     }
 }
